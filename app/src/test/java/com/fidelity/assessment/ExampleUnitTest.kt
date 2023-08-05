f8b65c1fc7baf2642f5fc638b53ec20da9c257ec
RaviCVG <cvg.ravikumarreddy@gmail.com>
2023-08-05 13:36:11 -0400

    Initial Commit

---
+++ app/src/test/java/com/fidelity/assessment/ExampleUnitTest.kt
@@ -0,0 +1,21 @@
+package com.fidelity.assessment
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
